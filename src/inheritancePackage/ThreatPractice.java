2c86417e289d7449fc95739c396ac856a00aef45
JordiMartinezDev <jordimartinezdev@gmail.com>
2024-06-05 06:56:16 +0000

    Thread name added

--- src/inheritancePackage/ThreatPractice.java
+++ src/inheritancePackage/ThreatPractice.java
@@ -7,8 +7,8 @@ public class ThreatPractice extends Thread{
     public void run(){
         System.out.println("Starting thread..");
         for(int i = 0; i < 10; i++){
-        System.out.println("Counting - "+ i);
-
+        System.out.println("Thead: "+ Thread.currentThread().getName() +" Counting - "+ i);
+        
         }
     }
 
